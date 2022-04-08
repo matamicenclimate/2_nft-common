7450885a8da65a10554c295b6ebd5db2251f7b66
pblanco-dekalabs <pblanco@dekalabs.com>
2022-04-08 09:46:03 +0200

    Splitted opt-in transaction creation and signing

--- src/services/OptInService.ts
+++ src/services/OptInService.ts
@@ -3,17 +3,20 @@ import * as WalletAccountProvider from './WalletAccountProvider'
 import Container, { Inject, Service } from 'typedi'
 import AlgodClientProvider from './AlgodClientProvider'
 import * as TransactionSigner from './TransactionSigner'
+import { TransactionOperation } from './TransactionOperation'
 
 @Service()
 export default class OptInService {
   readonly clientProvider: AlgodClientProvider
   readonly walletProvider: WalletAccountProvider.type
   readonly signer: TransactionSigner.type
+  readonly op: TransactionOperation
 
   constructor() {
     this.clientProvider = Container.get(AlgodClientProvider)
     this.walletProvider = WalletAccountProvider.get()
     this.signer = TransactionSigner.get()
+    this.op = Container.get(TransactionOperation)
   }
 
   private get client() {
@@ -24,11 +27,13 @@ export default class OptInService {
   static makeAssetTransferTransaction =
     algosdk.makeAssetTransferTxnWithSuggestedParams
 
-  async optInAssetByID(
-    assetID: number,
+  /**
+   * Prepares a raw asset opt-in transaction request.
+   */
+  async createOptInRequest(
+    assetId: number,
     sender: string = this.walletProvider.account.addr,
-    recipient = sender,
-    signer?: algosdk.Account
+    recipient = sender
   ) {
     const params = await this.client.getTransactionParams().do()
     const revocationTarget = undefined
@@ -36,26 +41,32 @@ export default class OptInService {
     const note = undefined
     const amount = 0
     console.log(`[OPT IN]\nsender = ${sender}\nrecipient = ${recipient}`)
-    const optInTxUnsigned = await OptInService.makeAssetTransferTransaction(
+    return await OptInService.makeAssetTransferTransaction(
       sender,
       recipient,
       closeRemainderTo,
       revocationTarget,
       amount,
       note,
-      assetID,
+      assetId,
       params
     )
-    const optInTxSigned =
-      signer != null
-        ? await optInTxUnsigned.signTxn(signer.sk)
-        : await this.signer.signTransaction(optInTxUnsigned)
-    const optInSendTx = await this.client.sendRawTransaction(optInTxSigned).do()
-    const confirmedOptInTx = await OptInService.waitForConfirmation(
-      this.client,
-      optInSendTx.txId,
-      4
+  }
+
+  /**
+   * Opts in the asset, signs the transaction.
+   */
+  async optInAssetByID(
+    assetId: number,
+    sender: string = this.walletProvider.account.addr,
+    recipient = sender,
+    signer?: algosdk.Account
+  ) {
+    const optInTxUnsigned = await this.createOptInRequest(
+      assetId,
+      sender,
+      recipient
     )
-    return confirmedOptInTx
+    return await this.op.signAndConfirm(optInTxUnsigned, undefined, signer)
   }
 }
