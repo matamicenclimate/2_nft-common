8d91ff3c25cb36e549a6a99cb8bdff883819519e
pblanco-dekalabs <pblanco@dekalabs.com>
2022-04-12 12:48:02 +0200

    stubbed api file.

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+/*
+  This module contains shared API type definitions.
+
+  The code presented here is intended to have the server
+  and the client in-sync (in schema terms) all the time.
+*/
+
+export type Zero = 0
