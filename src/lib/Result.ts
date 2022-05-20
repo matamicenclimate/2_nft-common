daea329c4fded7060f65c8aa18830104fd78a31e
pblanco-dekalabs <pblanco@dekalabs.com>
2022-05-20 10:27:52 +0200

    Abstracted result-like op.

---
+++ src/lib/Result.ts
@@ -0,0 +1,11 @@
+export class Failure {
+  failed = true as const
+  constructor(readonly reason: Error) {}
+}
+
+export class Success<A> {
+  failed = false as const
+  constructor(readonly result: A) {}
+}
+
+export type Result<A> = Failure | Success<A>
